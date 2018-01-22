39a8fed603efe64cca6aae950ca57c67fd5a1b7f
Hugh Hartigan <Hartigan.HM@gmail.com>
2018-01-22 14:28:24 -0700

    Refactor server.js to chain commands

--- server.js
+++ server.js
@@ -5,8 +5,8 @@ const path = require('path');
 app.set('port', process.env.PORT || 3000);
 app.locals.title = 'Palette Picker';
 
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.listen(app.get('port'), () => {
-  console.log(`${app.localts.title} is running on ${app.get('port')}.`);
-});
+app
+  .use(express.static(path.join(__dirname, 'public')))
+  .listen(app.get('port'), () => {
+    console.log(`${app.localts.title} is running on ${app.get('port')}.`);
+  });
