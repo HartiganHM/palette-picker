3d1ff6781d4bf165f96250ab615726e1392dd50c
Hugh Hartigan <Hartigan.HM@gmail.com>
2018-01-22 14:23:45 -0700

    server.js to use "public" directory for static assets

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const app = express();
+const path = require('path');
 
 app.set('port', process.env.PORT || 3000);
 app.locals.title = 'Palette Picker';
+
+app.use(express.static(path.join(__dirname, 'public')));
