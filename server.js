a12e0f0377ee47a899cf5fe102551923ff5793a7
Hugh Hartigan <Hartigan.HM@gmail.com>
2018-01-22 14:25:19 -0700

    server.js to listen on "port"

--- server.js
+++ server.js
@@ -6,3 +6,7 @@ app.set('port', process.env.PORT || 3000);
 app.locals.title = 'Palette Picker';
 
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.listen(app.get('port'), () => {
+  console.log(`${app.localts.title} is running on ${app.get('port')}.`);
+});
