492933b89976b7088ac3764e85220efac3dc3798
Hugh Hartigan <Hartigan.HM@gmail.com>
2018-01-22 21:55:48 -0700

    Add event listener to generate-palette-button

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -7,23 +7,43 @@ const generateColors = () => {
     { class: '.color-5', hexValue: '000000', locked: false }
   ];
 
-  const hexidecimalValues = [ 'A', 'B', 'C', 'D', 'E', 'F', 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 ];
+  const hexidecimalValues = [
+    'A',
+    'B',
+    'C',
+    'D',
+    'E',
+    'F',
+    0,
+    1,
+    2,
+    3,
+    4,
+    5,
+    6,
+    7,
+    8,
+    9
+  ];
 
   const colorValues = colorsArray.map(color => {
-    const hexValue = color.hexValue.split('').map(element => {
-      const randomValue = Math.floor(Math.random() * 16)
-      return hexidecimalValues[randomValue]
-    }).join('')
+    const hexValue = color.hexValue
+      .split('')
+      .map(element => {
+        const randomValue = Math.floor(Math.random() * 16);
+        return hexidecimalValues[randomValue];
+      })
+      .join('');
 
-    color.value = `#${hexValue}`
+    color.value = `#${hexValue}`;
     return color;
-  })
+  });
 
   colorValues.forEach(color => {
-    $(`${color.class}`).css('background-color', [color.value])
-    $(`${color.class}`).children()[1].innerText = `${color.value}`
-  })
-}
+    $(`${color.class}`).css('background-color', [color.value]);
+    $(`${color.class}`).children()[1].innerText = `${color.value}`;
+  });
+};
 
 $(document).ready(generateColors);
-$()
+$('.generate-palette-button').on('click', generateColors);
