950e51bf52305721154c3ee192d1283252a01984
Hugh Hartigan <Hartigan.HM@gmail.com>
2018-01-24 20:11:30 -0700

    Build schema for palette table

--- db/migrations/20180124200340_initial.js
+++ db/migrations/20180124200340_initial.js
@@ -1,4 +1,3 @@
-
 exports.up = function(knex, Promise) {
   return Promise.all([
     knex.schema.createTable('projects', function(table) {
@@ -7,9 +6,19 @@ exports.up = function(knex, Promise) {
 
       table.timestamps(true, true);
     }),
-  ]) // end Promise.all
+
+    knex.schema.createTable('palettes', function(table) {
+      table.increments('id').primary();
+      table.string('name');
+      table.string('color1');
+      table.string('color2');
+      table.string('color3');
+      table.string('color4');
+      table.string('color5');
+      table.integer('project_id').unsigned();
+      table.foreign('project_id').references('projects.id');
+    })
+  ]); // end Promise.all
 };
 
-exports.down = function(knex, Promise) {
-  
-};
+exports.down = function(knex, Promise) {};
