5e53671898e6c46ea9a3ddd33a2be6107b344458
Hugh Hartigan <Hartigan.HM@gmail.com>
2018-01-24 20:07:27 -0700

    Build schema for projects table

--- db/migrations/20180124200340_initial.js
+++ db/migrations/20180124200340_initial.js
@@ -1,6 +1,13 @@
 
 exports.up = function(knex, Promise) {
-  
+  return Promise.all([
+    knex.schema.createTable('projects', function(table) {
+      table.increments('id').primary();
+      table.string('name');
+
+      table.timestamps(true, true);
+    }),
+  ]) // end Promise.all
 };
 
 exports.down = function(knex, Promise) {
