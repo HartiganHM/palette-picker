81abb76d2c6c81340b1359842b3dcbeb9997e24b
Hugh Hartigan <Hartigan.HM@gmail.com>
2018-01-24 20:46:09 -0700

    Build GET method for projects endpoint

--- server.js
+++ server.js
@@ -8,13 +8,24 @@ const configuration = require('./knexfile')[environment];
 const database = require('knex')(configuration);
 const port = process.env.PORT || 3000;
 
-app.set('port', port);
 app.locals.title = 'Palette Picker';
+
+app.set('port', port);
 
 app
   .use(bodyParser.json())
   .use(bodyParser.urlencoded({ extended: true }))
   .use(express.static(path.join(__dirname, 'public')));
+
+app.get('/api/v1/projects', (request, response) => {
+  database('projects').select()
+    .then(projects => {
+      return response.status(200).json({ projects })
+    })
+    .catch(error => {
+      return response.status(500).json({ error });
+    });
+});
 
 app.listen(app.get('port'), () => {
   console.log(`${app.locals.title} is running on ${app.get('port')}.`);
