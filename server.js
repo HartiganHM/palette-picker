1ace574cc77c71243fab4e4f39f2ac49bcc1a7a8
Hugh Hartigan <Hartigan.HM@gmail.com>
2018-01-25 21:19:47 -0700

    Export app from server.js

--- server.js
+++ server.js
@@ -172,3 +172,5 @@ app.delete('/api/v1/palettes/:paletteId', (request, response) => {
 app.listen(app.get('port'), () => {
   console.log(`${app.locals.title} is running on ${app.get('port')}.`);
 });
+
+module.exports = app;
