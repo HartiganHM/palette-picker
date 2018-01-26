095f574e53c62c9490e892966c2e885702432597
Hugh Hartigan <Hartigan.HM@gmail.com>
2018-01-25 20:48:14 -0700

    Build project_test.js seed file

--- db/seeds/test/projects_test.js.js
+++ db/seeds/test/projects_test.js.js
@@ -1,13 +1,42 @@
-
 exports.seed = function(knex, Promise) {
-  // Deletes ALL existing entries
-  return knex('table_name').del()
-    .then(function () {
-      // Inserts seed entries
-      return knex('table_name').insert([
-        {id: 1, colName: 'rowValue1'},
-        {id: 2, colName: 'rowValue2'},
-        {id: 3, colName: 'rowValue3'}
-      ]);
-    });
+  return knex('palettes')
+    .del()
+    .then(() => knex('projects').del())
+
+    .then(() => {
+      return Promise.all([
+        knex('projects')
+          .insert(
+            {
+              name: 'Awesome'
+            },
+            'id'
+          )
+          .then(project => {
+            return knex('palettes').insert([
+              {
+                name: 'Blue',
+                color1: '#27A836',
+                color2: '#5FD185',
+                color3: '#D895C0',
+                color4: '#A22691',
+                color5: '#6F7703',
+                project_id: project[0]
+              },
+              {
+                name: 'Green',
+                color1: '#FFFFFF',
+                color2: '#5FD185',
+                color3: '#D895C0',
+                color4: '#A22691',
+                color5: '#000000',
+                project_id: project[0]
+              }
+            ]);
+          })
+          .then(() => console.log('Seeding complete!'))
+          .catch(error => console.log(`Error seeding data: ${error}`))
+      ]); // end Promise.all
+    })
+    .catch(error => console.log(`Error seeding data: ${error}`));
 };
