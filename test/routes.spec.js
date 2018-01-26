1e99680867c474e06c3983e6b72042fe24dfb39b
Hugh Hartigan <Hartigan.HM@gmail.com>
2018-01-26 06:08:24 -0700

    Build knex.js file in db directory

--- test/routes.spec.js
+++ test/routes.spec.js
@@ -4,7 +4,7 @@ const chai = require('chai');
 const should = chai.should();
 const chaiHttp = require('chai-http');
 const server = require('../server');
-const knex = require('../db/knex');
+const knex = require('knex');
 
 chai.use(chaiHttp);
 
