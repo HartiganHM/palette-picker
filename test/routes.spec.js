581db2fab170cd1a3ff9c9a1ad152c8fdbd36f25
Hugh Hartigan <Hartigan.HM@gmail.com>
2018-01-25 21:19:54 -0700

    Complete client side tests

--- test/routes.spec.js
+++ test/routes.spec.js
@@ -6,7 +6,28 @@ const server = require('../server');
 chai.use(chaiHttp);
 
 describe('Client Routes', () => {
+  it('Should return the homepage', () => {
+    return chai.request(server)
+    .get('/')
+    .then(response => {
+      response.should.have.status(200);
+      response.should.be.html;
+    })
+    .catch(error => {
+      throw error;
+    });
+  });
 
+  it('Should return a 404 for a route that does not exist', () => {
+    return chai.request(server)
+      .get('/sad')
+      .then(response => {
+        response.should.have.status(404);
+      })
+      .catch(error => {
+        throw error;
+      });
+  });
 });
 
 describe('API Routes', () => {
